463baabe00b87aaf5a6587afce550fb7d1320324
leekwansoo <leekwansoo49@gmail.com>
2025-02-10 10:15:34 +0900

    Update query_handler.py
    changed openai model

--- modules/query_handler.py
+++ modules/query_handler.py
@@ -10,7 +10,7 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 client=OpenAI()
 
 def answer_question(question, context):
-    chat_completion = client.chat.completions.create( messages=[ {"role": "user", "content": question} ], model="gpt-4o", ) 
+    chat_completion = client.chat.completions.create( messages=[ {"role": "user", "content": question} ], model="gpt-4o-mini", ) 
     return chat_completion.choices[0].message['content'].strip()
 
 def query_library(query):
@@ -23,6 +23,6 @@ def query_library(query):
     prompt = f"Given the context: {context}\n\nQ: {query}\nA:" 
     print(context)
     # Generate response using OpenAI 
-    chat_completion = client.chat.completions.create( messages=[ {"role": "user", "content": prompt} ], model="gpt-4o", ) 
+    chat_completion = client.chat.completions.create( messages=[ {"role": "user", "content": prompt} ], model="gpt-4o-mini", ) 
     
     return chat_completion.choices[0].message
